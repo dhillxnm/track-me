1faa31e2484cc37f7442a8decb0fef006377ac64
Manpreet <hcmanpreet2005@gmail.com>
2024-04-10 19:46:23 -0500

    javascript

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,22 +7,27 @@ import { onEvent, select, selectAll, create, print } from "./utility.js";
 const trackButton = select('.track-button');
 const mapInfo = select('.map-info');
 
+
 function getLocation(position){    
-    const {latitude, longitude} = position.coords;
- 
-    mapConfig(longitude, latitude);
+    const {latitude, longitude} = position.coords; // (coords) used to get info. about device location
+                                                   // we have just values og latitude and longitude
+    mapConfig(longitude, latitude);                // passing these values in the mapconfig gives device location in map
 }
  
+
 function errorHandler(){
     mapConfig(-97.19267,49.81486);
 }
  
+
 const options = {
     enableHighAccuracy: true
 }
  
+
 mapboxgl.accessToken = 'pk.eyJ1IjoieXV2cnhqc3IiLCJhIjoiY2xxM3g4a3NhMDE0bzJrbnZ6dGp6cmQwYSJ9.ko0Ddi2P09rnxx5TYkiSpQ';
  
+
 function mapConfig(longitude, latitude){
     const map = new mapboxgl.Map({
         container: 'map',
@@ -42,9 +47,11 @@ function mapConfig(longitude, latitude){
     .addTo(map);
 }
 
+
 function removeMapInfo() {
     mapInfo.innerText ='';
 }
+
 
 onEvent('click', trackButton, () => {
     if(navigator.geolocation) {
@@ -52,3 +59,9 @@ onEvent('click', trackButton, () => {
         navigator.geolocation.getCurrentPosition(getLocation, errorHandler, options);
     }
 });
+
+
+/*
+getCurrentPosition()
+method used to get latitude, longitute, altitute, accuracy of users device.
+*/
